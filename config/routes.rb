ef7dbd38aa8f1798b9495832a6a84939932afb1c
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-12-09 11:26:11 -0300

    Ajustes do layout do site / parte quem nao tem login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -8,7 +11,7 @@ Rails.application.routes.draw do
   end
   devise_for :profiles
   devise_for :admins
-  get 'welcome/index'
-  root to: 'welcome#index'
+  get 'site/welcome/index'
+  root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
