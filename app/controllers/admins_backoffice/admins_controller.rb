696945ce71fce59a5c8f33693f3d6e304aaea9f6
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-12-15 10:50:04 -0300

    form para criar um adm

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -6,13 +6,27 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
     @admins = Admin.all
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+
+    if @admin.save()
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
+      else
+        render :new
+    end
+  end
+
   def edit
     
   end
 
   def update
     if @admin.update(params_admin)
-      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
+      redirect_to admins_backoffice_admins_path, notice: "Administrador Cadastrado com sucesso!"
       else
         render :edit
     end
