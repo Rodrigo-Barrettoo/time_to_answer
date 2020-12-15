765b1c424949b5e32b220cd30dd63223f06c21ae
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-12-15 08:55:48 -0300

    form editacao de admins, e mostrando os erross

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
     get 'welcome/index'
-    resources :admins, only: [:index]
+    resources :admins, only: [:index, :edit, :update]
   end
 
   devise_for :admins
