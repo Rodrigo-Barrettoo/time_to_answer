5f0cd75825b35306f99dee6cf0cec4b703a4a5d7
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-12-22 10:24:44 -0300

    Filtrando as questões por assunto

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,15 @@
 module SiteHelper
+
+  def mdg_jumbotron
+
+    case params[:action]
+    when "index"
+      "Últimas perguntas cadastras..."
+    when "questions"
+      "Resutado para o termo: \"#{params[:term]}\"..."
+    when "subject"
+      "Mostrando questões com assunto: \"#{params[:subject]}\"..."
+    end
+
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -6,13 +6,19 @@ class Question < ApplicationRecord
   paginates_per 5
 
 
+  scope :search_subject, -> (page, subject_id) {
+    includes(:answers, :subject)
+      .where(subject_id: subject_id)
+      .page(page)
+  }
+
   scope :search, -> (page, term) {
-    includes(:answers)
+    includes(:answers, :subject)
       .where("lower(description) LIKE ?", "%#{term.downcase}%")
       .page(page)
   }
 
   scope :last_questions, -> (page) {
-    includes(:answers).order("created_at desc").page(page)
+    includes(:answers, :subject).order("created_at desc").page(page)
   }
 end
