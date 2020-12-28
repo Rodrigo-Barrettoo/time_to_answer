ce3f5d2eecffd42faa2fb92659478ceddb04ed13
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-12-28 14:58:18 -0300

    refatorando o UserStatistic

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,16 +1,6 @@
 class Site::AnswerController < SiteController
   def question
     @answer = Answer.find(params[:answer_id])
-
-    if user_signed_in?
-      user_statistic = UserStatistic.find_or_create_by(user: current_user)
-      if @answer.correct?
-        user_statistic.right_questions += 1
-      else
-        user_statistic.wrong_questions += 1
-      end
-
-      user_statistic.save
-    end
+    UserStatistic.set_statistic(@answer, current_user)
   end
 end
