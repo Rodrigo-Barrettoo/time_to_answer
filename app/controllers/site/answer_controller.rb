5ba5a94aaed4bb1e3ea39082cefecb011ba42298
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-12-28 14:49:24 -0300

    estatisticas do usuario

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,5 +1,16 @@
 class Site::AnswerController < SiteController
   def question
     @answer = Answer.find(params[:answer_id])
+
+    if user_signed_in?
+      user_statistic = UserStatistic.find_or_create_by(user: current_user)
+      if @answer.correct?
+        user_statistic.right_questions += 1
+      else
+        user_statistic.wrong_questions += 1
+      end
+
+      user_statistic.save
+    end
   end
 end
